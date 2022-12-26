17a21041d4221116f738ecf78ecb28fed67d9c5b
Ritesh <theritesh973@gmail.com>
2022-12-26 17:26:25 +0530

    CLL impl

---
+++ src/main/java/com/rits/cll/CircularLinkedList.java
@@ -0,0 +1,65 @@
+package com.rits.cll;
+
+import com.rits.common.Node;
+
+/**
+ * @author Ritesh Singh
+ * @version 1.0
+ * @since 26/12/22
+ */
+public class CircularLinkedList<Item> {
+
+    private Node tail;
+
+    public void insert(Item ele, Item data){
+        Node newNode = new Node(data);
+        if (tail == null){
+            tail = newNode;
+            newNode.setNext(newNode);
+
+        }else
+        {
+            Node current = tail;
+            while (!current.getItem().equals(ele)){
+                current = current.getNext();
+            }
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+
+
+        }
+
+    }
+    public void delete(Item data){
+
+            Node prev = tail;
+            Node current = prev.getNext();
+
+            while (!current.getItem().equals(data)){
+                prev = current;
+                current = current.getNext();
+            }
+            if (current == prev){
+                tail = null;
+            }
+            if (tail == current){
+                tail = prev;
+            }
+            prev.setNext(current.getNext());
+            current.setNext(null);
+
+
+    }
+    public void printList(){
+        Node myNode = tail;
+        if (tail == null){
+            System.out.println("List is empty");
+            return;
+        }
+        do {
+            System.out.println(tail.getItem() +" ");
+            tail = tail.getNext();
+        } while (tail != myNode);
+    }
+
+}
---
+++ src/main/java/com/rits/cll/CircularLinkedListDriver.java
@@ -0,0 +1,17 @@
+package com.rits.cll;
+
+/**
+ * @author Ritesh Singh
+ * @version 1.0
+ * @since 26/12/22
+ */
+public class CircularLinkedListDriver {
+    public static void main(String[] args) {
+        CircularLinkedList<Integer> cll = new CircularLinkedList<>();
+        cll.insert(1,20);
+        cll.insert(20,30);
+        cll.insert(20,40);
+        cll.delete(40);
+        cll.printList();
+    }
+}
